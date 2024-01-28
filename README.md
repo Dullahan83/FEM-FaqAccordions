46ec40271d6d0838edbc5fbd2833a437668e5d9d
Dullahan83 <maladrix@hotmail.com>
2024-01-28 14:12:20 +0100

    push

---
+++ README.md
@@ -0,0 +1,33 @@
+# Frontend Mentor - FAQ accordion solution
+
+This is a solution to the [FAQ accordion challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Links](#links)
+- [Built with](#built-with)
+
+### The challenge
+
+Users should be able to:
+
+- Hide/Show the answer to a question when the question is clicked
+- Navigate the questions and hide/show answers using keyboard navigation alone
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Links
+
+- Solution URL: [https://github.com/Dullahan83/FEM-FaqAccordions]
+- Live Site URL: [https://dullahan83.github.io/FEM-FaqAccordions/]
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Oriented Object Javascript
---
+++ index.js
@@ -0,0 +1,95 @@
+const arrObject = [
+  {
+    question: `What is Frontend Mentor, and how will it help me?`,
+    answer: `Frontend Mentor offers realistic coding challenges to help developers improve their 
+  frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
+  all levels and ideal for portfolio building.`,
+  },
+  {
+    question: `Is Frontend Mentor free?`,
+    answer: `Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+  option providing access to a range of projects suitable for all skill levels.`,
+  },
+  {
+    question: `Can I use Frontend Mentor projects in my portfolio?`,
+    answer: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+  way to showcase your skills to potential employers!`,
+  },
+  {
+    question: `How can I get help if I'm stuck on a challenge?`,
+    answer: `The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+  channel where you can ask questions and seek support from other community members.`,
+  },
+];
+
+class Accordion {
+  constructor(question, answer, initiallyOpen = false) {
+    this.directory = document.getElementById("faq-container");
+    this.button = null;
+    this.isOpen = initiallyOpen;
+    this.src;
+    this.ref;
+    this.contentHeight;
+    this.createAccordion(question, answer);
+  }
+  createAccordion(question, answer) {
+    const accordion = document.createElement("div");
+    accordion.classList.add("accordion");
+    this.directory.append(accordion);
+
+    // Create button element and bind it
+    const buttonElement = document.createElement("button");
+    this.button = buttonElement;
+    this.button.textContent = question;
+
+    const imgButtonElement = document.createElement("img");
+    imgButtonElement.src = "./assets/images/icon-plus.svg";
+
+    this.button.append(imgButtonElement);
+    accordion.append(this.button);
+
+    // Create accordion content
+    const divElement = document.createElement("div");
+    const textElement = document.createElement("p");
+    textElement.textContent = answer;
+    divElement.append(textElement);
+    accordion.append(divElement);
+
+    // Set the initial accordion height depending on initial state
+    this.ref = divElement;
+    this.contentHeight = textElement.getBoundingClientRect().height;
+    this.ref.style.height = this.isOpen ? `${this.contentHeight}px` : `0`;
+    this.initiateEvent();
+  }
+
+  toggleAccordion() {
+    this.isOpen = !this.isOpen;
+    const img = this.button.querySelector("img");
+    img.src = this.isOpen
+      ? "./assets/images/icon-minus.svg"
+      : "./assets/images/icon-plus.svg";
+    if (this.isOpen) {
+      this.ref.style.height = `${this.contentHeight}px`;
+    } else this.ref.style.height = `0`;
+  }
+
+  initiateEvent() {
+    this.button.addEventListener("click", () => this.toggleAccordion());
+    window.addEventListener("resize", () => this.onResize());
+  }
+
+  onResize() {
+    this.contentHeight = this.ref
+      .querySelector("p")
+      .getBoundingClientRect().height;
+    if (this.isOpen) {
+      this.ref.style.height = `${this.contentHeight}px`;
+    } else this.ref.style.height = `0`;
+  }
+}
+
+arrObject.map((data, i) => {
+  i === 0
+    ? new Accordion(data.question, data.answer, true)
+    : new Accordion(data.question, data.answer);
+});
